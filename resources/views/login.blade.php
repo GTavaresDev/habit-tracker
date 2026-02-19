58012b110a9854071ac3d74e0d02d0f22a4dc002
Tavares <tavares.gabriel.dev@gmail.com>
2026-02-19 10:57:08 -0300

    main
    Add authentication features: create LoginController, login view, and sessions table; update DatabaseSeeder to include UserSeeder.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+  <main class="py-10">
+  <h1>
+    login
+  </h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Rotas de login
+Route::get('/login', [LoginController::class, 'index']);
 
+//Rota de Dashboard
 Route::get('/home', [SiteController::class, 'index']);
